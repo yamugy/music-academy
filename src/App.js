9a72ee37c5a324be189c7dfa2e8a90575691b368
yamugy <yamugy72@gmail.com>
2024-11-16 03:25:47 +0900

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-<<<<<<< HEAD
-=======
-=======
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
->>>>>>> 8b4305ea2df8aa5b80341974ef0a46c81c39452c
->>>>>>> af54ef1e75cb24a9242382d86a7f608a300dba09
 import './styles/global.css';
 import { AuthProvider } from './contexts/AuthContext';
 import ProtectedRoute from './components/auth/ProtectedRoute';
@@ -18,13 +11,7 @@ import ClassManagement from './pages/ClassManagement';
 import PaymentManagement from './pages/PaymentManagement';
 import Login from './pages/Login';
 import PrintView from './pages/PrintView';
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
 import Courses from './pages/Courses';
->>>>>>> 8b4305ea2df8aa5b80341974ef0a46c81c39452c
->>>>>>> af54ef1e75cb24a9242382d86a7f608a300dba09
 
 function App() {
   return (
@@ -33,13 +20,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/print/:studentId" element={<PrintView />} />
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
           <Route path="/courses" element={<Courses />} />
->>>>>>> 8b4305ea2df8aa5b80341974ef0a46c81c39452c
->>>>>>> af54ef1e75cb24a9242382d86a7f608a300dba09
           <Route
             path="*"
             element={
